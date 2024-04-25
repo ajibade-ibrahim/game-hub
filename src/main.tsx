d8d247006c38fb56b46f23d794fa69b166c87f49
ajibade-ibrahim <ibrahim.ajibade@hotmail.com.com>
2024-04-25 09:51:51 +0100

    Refactor main.tsx to use ChakraProvider theme prop

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, ColorModeScript, extendTheme } from '@chakra-ui/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 import App from './App'
+import theme from './Theme'
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      {/* <ColorModeScript initialColorMode="dark" /> */}
       <App />
     </ChakraProvider>
   </React.StrictMode>
