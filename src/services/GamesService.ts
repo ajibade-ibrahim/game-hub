2a6fad23eae8b7cc6ba1d480c8fc1fe0398acb82
ajibade-ibrahim <ibrahim.ajibade@hotmail.com.com>
2024-04-25 15:34:25 +0100

    Added classes for network calls

---
+++ src/services/GamesService.ts
@@ -0,0 +1,28 @@
+import apiClient from './api-client'
+
+export interface Game {
+  id: number
+  name: string
+}
+
+export interface FetchGamesResponse {
+  count: number
+  next: string
+  previous: string
+  results: Game[]
+}
+
+class GamesService {
+  getGames(): Promise<Game[]> {
+    // TODO: Implement logic to fetch games from a data source using apiClient
+    return apiClient
+      .get<FetchGamesResponse>('/games')
+      .then((response) => response.data.results)
+      .catch((error) => {
+        console.error('Error fetching games:', error)
+        throw error
+      })
+  }
+}
+
+export default new GamesService()
