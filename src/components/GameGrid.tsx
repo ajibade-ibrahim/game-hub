9af2d419904b5da724168553e2e740814785986c
ajibade-ibrahim <ibrahim.ajibade@hotmail.com.com>
2024-04-26 09:32:46 +0100

    Implemented GameCardSkeleton

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,20 +1,26 @@
+import { SimpleGrid } from '@chakra-ui/react'
 import React from 'react'
 
 import useGames from '../hooks/useGames'
-import { SimpleGrid } from '@chakra-ui/react'
 import GameCard from './GameCard'
+import GameCardSkeleton from './GameCardSkeleton'
 
 const GameGrid: React.FC = () => {
-  const { games, error } = useGames()
+  const { games, error, isLoading } = useGames()
+  const numbers = Array.from({ length: 6 }, (_, index) => index + 1)
 
   return (
     <div>
       <h1>Game Grid</h1>
       {error && <p>{error}</p>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing={4}>
-        {games.map((game, index) => (
-          <GameCard key={index} game={game} />
-        ))}
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        padding="10px"
+        spacing={10}
+      >
+        {isLoading
+          ? numbers.map((number) => <GameCardSkeleton key={number} />)
+          : games.map((game, index) => <GameCard key={index} game={game} />)}
       </SimpleGrid>
     </div>
   )
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,27 +1,31 @@
 import { AxiosError } from 'axios'
-import { useState, useEffect } from 'react'
+import { useEffect, useState } from 'react'
+
 import GamesService, { Game } from '../services/GamesService'
 
 const useGames = () => {
   // State variables
   const [games, setGames] = useState<Game[]>([])
   const [error, setError] = useState<string | null>(null)
+  const [isLoading, setIsLoading] = useState<boolean>(true)
 
   useEffect(() => {
     const fetchGames = async () => {
       try {
         const gamesData = await GamesService.getGames()
         setGames(gamesData)
+        setIsLoading(false)
       } catch (error) {
         console.error('Error fetching games:', error)
         setError((error as AxiosError).message)
+        setIsLoading(false)
       }
     }
 
     fetchGames()
   }, [])
 
-  return { games, error }
+  return { games, error, isLoading }
 }
 
 export default useGames
