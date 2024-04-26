98a254b31b5f49c4276975083c7b23b92aed1fbf
ajibade-ibrahim <ibrahim.ajibade@hotmail.com.com>
2024-04-26 11:42:27 +0100

    Refactor GameCardSkeleton component to remove Card width prop

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -10,19 +10,14 @@ const GameGrid: React.FC = () => {
   const numbers = Array.from({ length: 6 }, (_, index) => index + 1)
 
   return (
-    <div>
-      <h1>Game Grid</h1>
+    <>
       {error && <p>{error}</p>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        padding="10px"
-        spacing={10}
-      >
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} padding="10px" spacing={3}>
         {isLoading
           ? numbers.map((number) => <GameCardSkeleton key={number} />)
           : games.map((game, index) => <GameCard key={index} game={game} />)}
       </SimpleGrid>
-    </div>
+    </>
   )
 }
 
