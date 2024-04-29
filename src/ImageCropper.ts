a9e5098762068fe6142fc4a6c359623eff0107f6
ajibade-ibrahim <ibrahim.ajibade@hotmail.com.com>
2024-04-29 14:59:32 +0100

    Added placeholder image for games with no image

--- src/ImageCropper.ts
+++ src/ImageCropper.ts
@@ -1,9 +1,10 @@
+import noImage from './assets/Image Placeholder/no-image-placeholder-6f3882e0.webp'
 export function getModifiedImageUrl(
   url: string,
   height: number = 600,
   width: number = 400
 ): string {
-  if (!url || url.length === 0) return url
+  if (!url || url.length === 0) return noImage
   const delimiter = '/media/'
   const split = url.split(delimiter)
   const modifiedUrl = `${split[0]}${delimiter}crop/${height}/${width}/${split[1]}`
